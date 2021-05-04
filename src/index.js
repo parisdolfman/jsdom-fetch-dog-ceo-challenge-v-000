14cea0c331da77798684dae8be28dc07504c7ab2
parisdolfman <parisdolfman@gmail.com>
2021-05-04 16:25:40 -0400

    commit tree e0eb5cbf1367145dc4184facb9ab9db48da253a9

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 console.log('%c HI', 'color: firebrick')
+
+let breeds = [];
+
+document.addEventListener('DOMContentLoaded', function () {
+    loadImages();
+    loadBreedOptions();
+  });
+
+function loadImages() {
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+  fetch(imgUrl)
+    .then(res=> res.json())
+    .then(results => {
+      results.message.forEach(image => addImage(image))
+    });
+}
